0d9e540bd67dae4bff258de0140c0832d9102dc4
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-09-29 23:55:48 +0900

    feat(utils): add `unsafe` function that handle dangerous code and convert to container

---
+++ dev_deps.ts
@@ -0,0 +1,2 @@
+export { assertEquals } from "https://deno.land/std@0.157.0/testing/asserts.ts";
+export { describe, it } from "https://deno.land/std@0.157.0/testing/bdd.ts";
---
+++ utils_test.ts
@@ -0,0 +1,33 @@
+import { unsafe } from "./utils.ts";
+import { assertEquals, describe, it } from "./dev_deps.ts";
+
+describe("unsafe", () => {
+  it("should return err container when the function throw error", () => {
+    const result = unsafe(() => {
+      throw Error();
+    });
+    assertEquals(result.value, Error());
+    assertEquals(result.isOk(), false);
+  });
+
+  it("should return err container when the function throw any error", () => {
+    const result = unsafe<Headers, TypeError>(() =>
+      new Headers({ "?": "invalid field name" })
+    );
+    assertEquals(result.value, new TypeError());
+  });
+
+  it("should return ok container when the function return", () => {
+    const result = unsafe(() => {
+      return 0;
+    });
+    assertEquals(result.value, 0);
+    assertEquals(result.isOk(), true);
+  });
+
+  it("should return ok container when the function return undefined", () => {
+    const result = unsafe(() => {});
+    assertEquals(result.value, undefined);
+    assertEquals(result.isOk(), true);
+  });
+});
