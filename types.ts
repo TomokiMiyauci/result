583faa4a790a63a2f2ef84f5b17b6a14396de385
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-09-30 11:27:41 +0900

    feat(types): re-define container types

---
+++ types.ts
@@ -0,0 +1,38 @@
+/** Container API. */
+export interface Container {
+  /** The contained value. */
+  value: unknown;
+
+  /** The container type. */
+  type: string;
+}
+
+/** Ok container API. */
+export interface OkContainer<T> extends Container {
+  /** Ok container value. */
+  value: T;
+
+  /** Ok container type. */
+  type: "ok";
+}
+
+/** Err container API. */
+export interface ErrContainer<E> extends Container {
+  /** Err container value. */
+  value: E;
+
+  /** Err container type. */
+  type: "err";
+}
+
+/** Result container API. */
+export type ResultContainer<T, E> = OkContainer<T> | ErrContainer<E>;
+
+/** Result container constructor. */
+export interface ResultConstructor {
+  /** Create a new {@link OkContainer}. */
+  ok: <T>(value: T) => OkContainer<T>;
+
+  /** Create a new {@link ErrContainer}. */
+  err: <E>(value: E) => ErrContainer<E>;
+}
