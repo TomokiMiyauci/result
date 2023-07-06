0919773137c0a7cf9c6270b5ed5bc34cf4b1ccb5
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-07-06 23:42:03 +0900

    feat(mod): remove all API
    BREAKING CHANGE: remove all API

--- mod.ts
+++ mod.ts
@@ -1,12 +1,2 @@
 // Copyright 2022-latest Tomoki Miyauchi. All rights reserved. MIT license.
 // This module is browser compatible.
-
-export { Result } from "./results.ts";
-export {
-  type Container,
-  type ErrContainer,
-  type OkContainer,
-  type ResultConstructor,
-  type ResultContainer,
-} from "./types.ts";
-export { isErr, isOk, match, type Patterns, unsafe } from "./utils.ts";
