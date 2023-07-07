e033f82713df356d3b9d8dffc69de424051a22e4
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-07-07 13:49:45 +0900

    feat(mod): export related modules

--- _dev_deps.ts
+++ _dev_deps.ts
@@ -1,7 +1,15 @@
-export { assertEquals } from "https://deno.land/std@0.157.0/testing/asserts.ts";
+// Copyright © 2023 Tomoki Miyauchi. All rights reserved. MIT license.
+// This module is browser compatible.
+
 export {
-  assertSpyCall,
+  assert,
+  assertEquals,
+  assertFalse,
+  assertThrows,
+} from "https://deno.land/std@0.190.0/testing/asserts.ts";
+export { describe, it } from "https://deno.land/std@0.190.0/testing/bdd.ts";
+export {
+  assertSpyCallArgs,
   assertSpyCalls,
   spy,
-} from "https://deno.land/std@0.157.0/testing/mock.ts";
-export { describe, it } from "https://deno.land/std@0.157.0/testing/bdd.ts";
+} from "https://deno.land/std@0.190.0/testing/mock.ts";
--- mod.ts
+++ mod.ts
@@ -1,2 +1,24 @@
-// Copyright 2022-latest Tomoki Miyauchi. All rights reserved. MIT license.
+// Copyright © 2023 Tomoki Miyauchi. All rights reserved. MIT license.
 // This module is browser compatible.
+
+export {
+  Err,
+  type ErrConstructor,
+  Ok,
+  type OkConstructor,
+  type Result,
+  ResultType,
+} from "./spec.ts";
+export { isErr, isOk } from "./operators/query.ts";
+export { and, andThen, or, orElse } from "./operators/logical.ts";
+export { map, mapOr, mapOrElse } from "./operators/transform.ts";
+export {
+  expect,
+  expectErr,
+  match,
+  type Matcher,
+  unwrap,
+  unwrapErr,
+  unwrapOr,
+  unwrapOrElse,
+} from "./operators/extract.ts";
