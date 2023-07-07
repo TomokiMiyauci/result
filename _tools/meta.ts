f4c376bcb90755e2bc37c753831d6dbb63c7cf96
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-07-07 14:52:19 +0900

    chore(_tools): update npm build meta

--- _tools/meta.ts
+++ _tools/meta.ts
@@ -1,26 +1,21 @@
-import { BuildOptions } from "https://deno.land/x/dnt@0.30.0/mod.ts";
+import { BuildOptions } from "https://deno.land/x/dnt@0.37.0/mod.ts";
 
 export const makeOptions = (version: string): BuildOptions => ({
   test: false,
   shims: {},
-  typeCheck: true,
+  typeCheck: "both",
   entryPoints: ["./mod.ts"],
   outDir: "./npm",
   package: {
     name: "@miyauci/result",
     version,
-    description: "The standard API for result in JavaScript",
+    description: "Minimum result type port of Rust",
     keywords: [
       "result",
       "ok",
       "error",
-      "container",
-      "handle",
-      "throw",
-      "throws",
-      "handling",
-      "match",
-      "unsafe",
+      "success",
+      "failure",
     ],
     license: "MIT",
     homepage: "https://github.com/TomokiMiyauci/result-js",
