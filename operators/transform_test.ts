fb5c25980028a4832d2d5cc0f26bc388cf11d0d6
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-07-07 16:00:09 +0900

    chore: rename arg

--- operators/transform_test.ts
+++ operators/transform_test.ts
@@ -14,21 +14,21 @@ import {
 describe("map", () => {
   it("should call mapper if it is Ok", () => {
     const INPUT = "Hello, World!";
-    const option: Ok<string> = Ok(INPUT);
+    const result: Ok<string> = Ok(INPUT);
     const fn = spy((v: string) => v.length);
-    const optionLen = map(option, fn);
+    const resultLen = map(result, fn);
 
-    assertEquals(optionLen, Ok(13));
+    assertEquals(resultLen, Ok(13));
     assertSpyCalls(fn, 1);
     assertSpyCallArgs(fn, 0, [INPUT]);
   });
 
   it("should not call mapper function if it is Err", () => {
-    const option: Err<string> = Err("");
+    const result: Err<string> = Err("");
     const fn = spy((v: string) => v.length);
-    const optionLen = map(option, fn);
+    const resultLen = map(result, fn);
 
-    assertEquals(optionLen, Err(""));
+    assertEquals(resultLen, Err(""));
     assertSpyCalls(fn, 0);
   });
 });
@@ -36,11 +36,11 @@ describe("map", () => {
 describe("mapErr", () => {
   it("should call mapper if it is Err", () => {
     const INPUT = "Hello, World!";
-    const option: Err<string> = Err(INPUT);
+    const result: Err<string> = Err(INPUT);
     const fn = spy((v: string) => v.length);
-    const optionLen = mapErr(option, fn);
+    const resultLen = mapErr(result, fn);
 
-    assertEquals(optionLen, Err(13));
+    assertEquals(resultLen, Err(13));
     assertSpyCalls(fn, 1);
     assertSpyCallArgs(fn, 0, [INPUT]);
   });
@@ -48,9 +48,9 @@ describe("mapErr", () => {
   it("should not call mapper function if it is Ok", () => {
     const result: Ok<number> = Ok(0);
     const fn = spy((v: string) => v.length);
-    const optionLen = mapErr(result, fn);
+    const resultLen = mapErr(result, fn);
 
-    assertEquals(optionLen, Ok(0));
+    assertEquals(resultLen, Ok(0));
     assertSpyCalls(fn, 0);
   });
 });
@@ -58,21 +58,21 @@ describe("mapErr", () => {
 describe("mapOr", () => {
   it("should call mapper if it is Ok", () => {
     const INPUT = "Hello, World!";
-    const option: Ok<string> = Ok(INPUT);
+    const result: Ok<string> = Ok(INPUT);
     const fn = spy((v: string) => v.length);
-    const optionLen = mapOr(option, 0, fn);
+    const resultLen = mapOr(result, 0, fn);
 
-    assertEquals(optionLen, INPUT.length);
+    assertEquals(resultLen, INPUT.length);
     assertSpyCalls(fn, 1);
     assertSpyCallArgs(fn, 0, [INPUT]);
   });
 
   it("should return default value if it is Err", () => {
-    const option: Err<string> = Err("");
+    const result: Err<string> = Err("");
     const fn = spy((v: string) => v.length);
-    const optionLen = mapOr(option, 0, fn);
+    const resultLen = mapOr(result, 0, fn);
 
-    assertEquals(optionLen, 0);
+    assertEquals(resultLen, 0);
     assertSpyCalls(fn, 0);
   });
 });
@@ -80,24 +80,24 @@ describe("mapOr", () => {
 describe("mapOrElse", () => {
   it("should call mapper if it is Ok", () => {
     const INPUT = "Hello, World!";
-    const option: Ok<string> = Ok(INPUT);
+    const result: Ok<string> = Ok(INPUT);
     const fn = spy((v: string) => v.length);
     const defaultFn = spy(() => 0);
-    const optionLen = mapOrElse(option, defaultFn, fn);
+    const resultLen = mapOrElse(result, defaultFn, fn);
 
-    assertEquals(optionLen, INPUT.length);
+    assertEquals(resultLen, INPUT.length);
     assertSpyCalls(fn, 1);
     assertSpyCallArgs(fn, 0, [INPUT]);
     assertSpyCalls(defaultFn, 0);
   });
 
   it("should return default value if it is Err", () => {
-    const option: Err<string> = Err("");
+    const result: Err<string> = Err("");
     const fn = spy((v: string) => v.length);
     const defaultFn = spy(() => 0);
-    const optionLen = mapOrElse(option, defaultFn, fn);
+    const resultLen = mapOrElse(result, defaultFn, fn);
 
-    assertEquals(optionLen, 0);
+    assertEquals(resultLen, 0);
     assertSpyCalls(fn, 0);
     assertSpyCalls(defaultFn, 1);
   });
